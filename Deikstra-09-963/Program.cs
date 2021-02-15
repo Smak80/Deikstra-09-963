752c3b74ff4ba24531e9bb1a1fab3b3c0ed5cd7d
Smak80 <smaklets@kpfu.ru>
2021-02-15 14:03:40 +0300

    Добавьте файлы проекта.

---
+++ Deikstra-09-963/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deikstra_09_963
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var data = Loader.LoadData("data.csv");
+                Console.WriteLine(data[3, 2]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Проблема с чтением из файла :(");
+            }
+        }
+    }
+}
---
+++ Deikstra-09-963/Vertex.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deikstra_09_963
+{
+    class Vertex
+    {
+        private double minPathLength = double.PositiveInfinity;
+        private Vertex prev = null;
+        private int num;
+        private static int count = 0;
+
+        public int Num
+        {
+            get { return num; }
+            private set
+            {
+                num = value;
+            }
+        }
+
+        public Vertex()
+        {
+            count++;
+            Num = count;
+        }
+
+        public static void ResetVerticies()
+        {
+            count = 0;
+        }
+    }
+}
