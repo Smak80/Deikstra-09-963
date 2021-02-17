83dacac17c37d1ea85cefc4fd112f5b52e66f4bd
Smak80 <smaklets@kpfu.ru>
2021-02-17 08:34:02 +0300

    Исходный вариант

--- Deikstra-09-963/Program.cs
+++ Deikstra-09-963/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,39 @@ namespace Deikstra_09_963
 {
     class Program
     {
+
+        static void ShowPath(List<Vertex> path)
+        {
+            foreach (var v in path)
+            {
+                Console.Write("{0} ", v.Num);
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
             try
             {
                 var data = Loader.LoadData("data.csv");
-                Console.WriteLine(data[3, 2]);
+                Graph g = new Graph(data);
+                int b = 1;
+                int e = 6;
+                var path = g.GetShortestPath(b, e);
+                ShowPath(path);
+                Console.WriteLine("Длина кратчайшего пути: {0}", Graph.GetShortestPathLength(path));
+                b = 2;
+                path = g.GetShortestPath(b, e);
+                ShowPath(path);
+                Console.WriteLine("Длина кратчайшего пути: {0}", Graph.GetShortestPathLength(path));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Проблема с чтением из файла :(");
             }
             catch (Exception e)
             {
-                Console.WriteLine("Проблема с чтением из файла :(");
+                Console.WriteLine("Что-то пошло не так :(");
             }
         }
     }
--- Deikstra-09-963/Vertex.cs
+++ Deikstra-09-963/Vertex.cs
@@ -8,10 +8,27 @@ namespace Deikstra_09_963
 {
     class Vertex
     {
-        private double minPathLength = double.PositiveInfinity;
-        private Vertex prev = null;
+
+        private double minPathLength;
+        private Vertex prev;
         private int num;
         private static int count = 0;
+        private bool visited;
+
+        public void SetVisited()
+        {
+            visited = true;
+        }
+
+        public bool IsVisited
+        {
+            get => visited;
+        }
+
+        public double Label
+        {
+            get => minPathLength;
+        }
 
         public int Num
         {
@@ -26,11 +43,41 @@ namespace Deikstra_09_963
         {
             count++;
             Num = count;
+            Reset();
         }
 
-        public static void ResetVerticies()
+        public static void ResetCounter()
         {
             count = 0;
         }
+
+        public void Reset()
+        {
+            minPathLength = double.PositiveInfinity;
+            prev = null;
+            visited = false;
+        }
+
+        public void SetStart()
+        {
+            minPathLength = 0;
+        }
+
+        public Vertex Previous
+        {
+            get => prev;
+        }
+
+        public void SetPrevious(Vertex prev, double edge)
+        {
+            if (visited) return;
+            var length = prev.minPathLength + edge;
+            if (minPathLength > length)
+            {
+                minPathLength = length;
+                this.prev = prev;
+            }
+        }
+
     }
 }
